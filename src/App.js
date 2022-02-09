21ed88758fb1fecd6f5079b02b9c10b706df381e
Adriana Mucciolo <drishaolin@gmail.com>
2022-02-08 21:45:17 -0300

    controlled inputs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [messageList, setMessageList] = useState([]);
-    
+    const [authorEmail, setAuthorEmail] = useState("");
+    const [messageContent, setMessageContent] = useState("");
 
     useEffect(() => {
         fetch("http://localhost:5000/message")
             .then((res) => res.json())
             .then((json) => setMessageList(json));
     }, []);
+
+    function sendMessage() {
+        //TODO validate data, fetch to post data
+        console.log("post");
+    }
 
     return (
         <>
@@ -17,13 +23,15 @@ function App() {
                 <h2 className="fs-4">Mural de Mensagens</h2>
                 <div className="mb-3" style={{ width: "50rem" }}>
                     <label htmlFor="nameInput" className="form-label">
-                        Nome
+                        Email
                     </label>
                     <input
-                        type="text"
+                        value={authorEmail}
+                        onChange={({target})=> setAuthorEmail(target.value)}
+                        type="email"
                         className="form-control"
-                        id="nameInput"
-                        placeholder="Digite seu nome"
+                        id="emailInput"
+                        placeholder="email@exemplo.com"
                     />
                 </div>
                 <div className="mb-3" style={{ width: "50rem" }}>
@@ -31,27 +39,28 @@ function App() {
                         Mensagem
                     </label>
                     <textarea
+                    value={messageContent}
+                    onChange={({target})=> setMessageContent(target.value)}
                         className="form-control"
                         id="messageInput"
                         rows="3"
                         placeholder="Digite sua mensagem"
                     ></textarea>
                 </div>
-                <button type="button" className="btn btn-primary">
+                <button type="button" className="btn btn-primary" onClick={sendMessage}>
                     Enviar
                 </button>
 
                 <h3 className="fs-4 mt-4">Últimas mensagens enviadas: </h3>
-                {messageList.map(({id, message, email, created_at}, index) => 
-                <div key={id} className="card my-3" style={{ width: "50rem" }}>
-                    <div className="card-body">
-                        <h5 className="card-title">{email}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{created_at}</h6>
-                        <p className="card-text">{message}</p>
+                {messageList.map(({ id, message, email, created_at }, index) => (
+                    <div key={id} className="card my-3" style={{ width: "50rem" }}>
+                        <div className="card-body">
+                            <h5 className="card-title">{email}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{created_at}</h6>
+                            <p className="card-text">{message}</p>
+                        </div>
                     </div>
-                </div>
-                
-                )}
+                ))}
             </div>
         </>
     );
